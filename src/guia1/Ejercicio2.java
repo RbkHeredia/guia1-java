daaf3f382db4e489f2424483f2dd657eb2d795d7
Rebeca Heredia <rbkheredia90@gmail.com>
2023-05-24 21:35:00 -0300

    ejercicio2

---
+++ src/guia1/Ejercicio2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia1;
+
+/**
+ *
+ * @author rebek
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         //agregar valores a las variables
+        String name = "Rebeca";
+        int numberPhone = 0303456;
+        boolean isTall = false;
+        float height = (float) 1.60;
+        char lastName = 'R';
+        String pets = "A dog";
+        
+        //detección de errores
+        int numero = 48;
+        double decimales = 3.55;
+        boolean bandera = false;
+    }
+    
+}
